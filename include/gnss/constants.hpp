83e00ff97f96c337d2a57248b9be18e735a0686b
TheRandomWalk.org <78839864+TheRandomWalk@users.noreply.github.com>
2026-07-02 01:41:05 -0400

    Add GNSS constants

---
+++ include/gnss/constants.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace gnss {
+
+inline constexpr double kSpeedOfLightMps = 299792458.0;
+inline constexpr double kGpsL1Hz = 1575.42e6;
+inline constexpr double kGpsCaCodeRateHz = 1.023e6;
+inline constexpr int kGpsCaCodeLength = 1023;
+inline constexpr double kPi = 3.141592653589793238462643383279502884;
+
+} // namespace gnss
